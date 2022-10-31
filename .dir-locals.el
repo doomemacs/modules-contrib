68da88e6436d04631a58fcebfd0f2c966c782b81
Henrik Lissner <git@henrik.io>
2022-10-31 16:59:09 +0100

    fix: update .dir-locals.el to reflect recent changes
    Much has changed in doom-docs, upstream.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,15 +1,3 @@
-((nil
-  (git-commit-major-mode . git-commit-elisp-text-mode)
-  (fill-column . 80))
- (doom-docs-org-mode
-  (doom-docs-repo "https://git.doomemacs.org/contrib-modules")
-  (doom-docs-menus
-   ("/README\\.org$"
-    (left  ("← Back: core" . "doomemacs:core")))
-   ("/[^/]+/README\\.org$"
-    (left  ("← Back: module index" . "file:../README.org")))
-   ("/\\([^/]+\\)/\\([^/]+\\)/README\\.org$"
-    (left  ("← Back: module index" . "file:../../README.org")
-           ("↑ Up: \\1" . "file:../README.org"))
-    (right ("↖ History" . "github-commits:./")
-           ("! Issues"  . "github-label:module:\\1/\\2"))))))
+((nil (git-commit-major-mode . git-commit-elisp-text-mode)
+      (fill-column . 80))
+ (org-mode (mode . doom-docs-org)))
